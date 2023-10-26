f71738a12978e30f4e5dc82d8d62f5a48217d058
Djin <32814285+jdnejal@users.noreply.github.com>
2023-10-27 02:44:48 +0900

    Load Custom 3D Model

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,46 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const color1 = new THREE.Color();
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.background = color1;
+scene.add( light );
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Cube model
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//scene.add( cube );
+
+// Load own model
+const loader = new GLTFLoader();
+
+loader.load( 'src/assets/13_Can.gltf', function ( gltf ) {
+
+  const canModel = gltf.scene;
+	scene.add( canModel );
+
+  function animate() {
+    requestAnimationFrame( animate );
+    canModel.rotation.y += 0.01
+    renderer.render( scene, camera );
+  }
+  animate();
+ 
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
 
 camera.position.z = 5;
 
