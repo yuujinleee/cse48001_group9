3f2848d2177be0cfe3c12302dcd650ac8b290dbe
Djin <32814285+jdnejal@users.noreply.github.com>
2023-10-29 23:51:59 +0900

    Interactable 3d model
    Mouse can now be used to rotate object

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import './index.css'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -21,7 +23,7 @@ document.body.appendChild( renderer.domElement );
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-//scene.add( cube );
+// scene.add( cube );
 
 // Load own model
 const loader = new GLTFLoader();
@@ -30,13 +32,8 @@ loader.load( 'src/assets/13_Can.gltf', function ( gltf ) {
 
   const canModel = gltf.scene;
 	scene.add( canModel );
-
-  function animate() {
-    requestAnimationFrame( animate );
-    canModel.rotation.y += 0.01
-    renderer.render( scene, camera );
-  }
-  animate();
+  const controls = new OrbitControls( camera, renderer.domElement );
+  controls.enableDamping = false
  
 }, undefined, function ( error ) {
 
