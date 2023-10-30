7d3567df9a29b03f1748c0204176317cf9cbf1c6
Djin <32814285+jdnejal@users.noreply.github.com>
2023-10-30 14:19:21 +0900

    Supabase connection established

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-//import ModelLoader from './components/ModelLoader'
 
 function App() {
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-
+//-------------- Code for 3D rendering --------------//
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const color1 = new THREE.Color();
@@ -41,6 +42,29 @@ function animate() {
   renderer.render( scene, camera );
 }
 animate();
+
+
+//-------------- Code for Database connection --------------//
+import { createClient } from '@supabase/supabase-js'
+import { Database } from './database/database.types.ts'
+
+// URL and KEY
+const SUPABASE_URL = 'https://sssfwtibjhdthffelfip.supabase.co'
+const SUPABASE_ANON_KEY= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzc2Z3dGliamhkdGhmZmVsZmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg1OTIyOTUsImV4cCI6MjAxNDE2ODI5NX0.anAxyqjmgv6qIo-tiFSRvrOrona2OWCTpyNpwbDuu8M'
+
+// Connect to database
+const supabase = createClient<Database>(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+)
+
+
+// Fetch data
+const { data } = await supabase
+.from('countries')
+.select('name')
+
+console.log(data)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
