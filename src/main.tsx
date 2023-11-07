ae2d6efb0830cd30853844b8c012f5e21a48d927
Yujin Lee <yujinlee@unist.ac.kr>
2023-11-07 17:58:04 +0900

    added sample annotation

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 
-let scene, camera, renderer, controls;
-
-let sprite, spriteBehindObject; 
+let scene, camera, renderer, controls, model;
+let sprite, spriteBehindObject;
 const annotation = document.querySelector(".annotation");
 
 init();
@@ -59,17 +58,35 @@ function init() {
 
 
 
-
   //-------------- Geometries --------------//
   // Load 3d model
   new GLTFLoader().load( 'src/assets/13_Can.gltf', function ( gltf ) {
-    const model = gltf.scene;
+    model = gltf.scene;
     scene.add( model );
   }, undefined, function ( error ) {
     console.error( error );
   } );
 
+  // Sprite
+    const numberTexture = new THREE.CanvasTexture(
+        document.querySelector("#number")
+    );
 
+    const spriteMaterial = new THREE.SpriteMaterial({
+        map: numberTexture,
+        alphaTest: 0.5,
+        transparent: true,
+        depthTest: false,
+        depthWrite: false
+    });
+
+    sprite = new THREE.Sprite(spriteMaterial);
+    sprite.position.set(1, 1, 1);
+    sprite.scale.set(60, 60, 1);
+
+    scene.add(sprite);
+
+    console.log(model)
 
   window.addEventListener("resize", onWindowResize, false);
 }
@@ -84,16 +101,41 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
-    // console.log(camera.position)
     render();
 }
 
 function render() {
     renderer.render(scene, camera);
-    // updateAnnotationOpacity();
-    // updateScreenPosition();
+    updateAnnotationOpacity();
+    updateScreenPosition();
 }
 
+function updateAnnotationOpacity() {
+    const meshDistance = camera.position.distanceTo(model.position);
+    const spriteDistance = camera.position.distanceTo(sprite.position);
+    spriteBehindObject = spriteDistance > meshDistance;
+    sprite.material.opacity = spriteBehindObject ? 0.25 : 1;
+
+    // Do you want a number that changes size according to its position?
+    // Comment out the following line and the `::before` pseudo-element.
+    sprite.material.opacity = 0;
+}
+
+function updateScreenPosition() {
+    const vector = new THREE.Vector3(0.2,0.85,0.2);
+    const canvas = renderer.domElement;
+
+    vector.project(camera);
+
+    vector.x = Math.round((0.5 + vector.x / 2) * (canvas.width / window.devicePixelRatio));
+    vector.y = Math.round((0.5 - vector.y / 2) * (canvas.height / window.devicePixelRatio));
+
+    if (annotation){
+      annotation.style.top = `${vector.y}px`;
+      annotation.style.left = `${vector.x}px`;
+      annotation.style.opacity = spriteBehindObject ? 0.25 : 1;
+    }
+}
 
 
 //-------------- Code for Database connection --------------//
