5303f9a0ce078fe77682c6ddf541fc316f71f6f4
Yujin Lee <yujinlee@unist.ac.kr>
2023-11-18 16:32:32 +0900

    added annotation popup react form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import AnnotationEditor from "./components/AnnotationEditor";
+import AnnotationPopup from "./components/AnnotationPopup";
 
 function App() {
   return (
     <div id="three">
+      <AnnotationPopup />
       <AnnotationEditor />
     </div>
   );
