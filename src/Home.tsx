6d67f717127783cd66f29a31d67629178ba3ba34
Djin <32814285+jdnejal@users.noreply.github.com>
2023-11-21 23:05:21 +0900

    Just some testing

---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.css'
+
+
+function Home() {
+
+  return (
+    <div className='container'>
+        <Button variant='primary' size='lg' >Create a session</Button>
+    </div>
+  )
+}
+
+export default Home
