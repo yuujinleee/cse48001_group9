2a079fd928f7e7932223f23f09e5d40abb9c7530
Djin <32814285+jdnejal@users.noreply.github.com>
2023-11-22 13:21:43 +0900

    Basic homescreen layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,28 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 import './Home.css'
 
 
 function Home() {
 
   return (
-    <div className='container'>
-        <Button variant='primary' size='lg' >Create a session</Button>
-    </div>
+    <Container>
+        <Row className='logoBoxContainer'>
+            LOGO
+        </Row>
+        <Row className='sessionNameInputBoxContainer'>
+            Session name
+        </Row>
+        <Row className='dragAndDropInputBoxContainer'>
+            Drag and drop to upload file
+        </Row>
+        <Row className='createSessionButtonContainer'>
+            <Button variant='primary' size='lg' className='createSessionButton' >Create a session</Button>
+        </Row>
+    </Container>
   )
 }
 
--- src/homescreen.tsx
+++ src/homescreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
