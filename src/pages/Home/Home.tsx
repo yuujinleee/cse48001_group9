b8d8b77b0d77934d370c40969d60ea15ac909c63
Djin <32814285+jdnejal@users.noreply.github.com>
2023-11-30 11:17:30 +0900

    Added base layout for new homepage
    Moved old home page to start page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,18 +12,9 @@ function Home() {
   return (
         <div className='content'>
             <Container>
-                <Button variant='primary' className='buttonTopRight'>Dashboard</Button>
+                <Button variant='primary' className='buttonTopRight'>Login</Button>
                 <Row className='logoBoxContainer'>
                     <img src={LogoSVG} style={{ width: '350px', height: '175px' }} alt="Logo" />
-                </Row>
-                <Row className='sessionNameInputBoxContainer'>
-                    <input type="text" placeholder="Enter session name" className='sessionInputBox'/>
-                </Row>
-                <Row className='dragAndDropInputBoxContainer'>
-                    Drag and drop to upload file
-                </Row>
-                <Row className='createSessionButtonContainer'>
-                    <Button variant='primary' size='lg' className='createSessionButton' >Create a session</Button>
                 </Row>
             </Container>
         </div>
