5a2cb5d0a53c89c456659fd10aec33eda976708b
Djin <32814285+jdnejal@users.noreply.github.com>
2023-11-30 11:38:59 +0900

    Base layout of main page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import LogoSVG from '../../assets/Pixey3D_logo.svg'
 
@@ -16,6 +17,27 @@ function Home() {
                 <Row className='logoBoxContainer'>
                     <img src={LogoSVG} style={{ width: '350px', height: '175px' }} alt="Logo" />
                 </Row>
+                <Row className='sloganBox'>
+                        The web application to support feedback interaction in 3D-modeling.
+                </Row>
+                <Row>
+                    <Col className='textBox'>
+                        PUT TEXT HERE
+                    </Col>
+                    <Col className='videoBox'>
+                        PUT video HERE
+                    </Col>
+                </Row>
+
+                <Row style={{marginTop: '100px'}}>
+                    <Col className='videoBox'>
+                        PUT TEXT HERE
+                    </Col>
+                    <Col className='textBox'>
+                        PUT video HERE
+                    </Col>
+                </Row>
+
             </Container>
         </div>
   )
